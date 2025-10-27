d5e4120d1dc309090243943f16d06a374ef7cd25
stonedjjh <danjim82@gmail.com>
2025-10-27 06:33:35 -0400

    Chore: Add initial setup for exercise visualization
    Adds the necessary files (index.html, styles.css, main.ts)
    to import and display Clean Code exercises.

---
+++ ejercicios/main.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+//import "./clean-code/07-tarea";
+import "./solid/02-open-close-a";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+  `;
